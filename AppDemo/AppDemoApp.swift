fb2894b332e7e1dda811820d188de61729263a40
SalemGen-c <mohamed.salem@gen-c.com>
2022-02-24 20:43:09 +0200

    Initial Commit

---
+++ AppDemo/AppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDemoApp.swift
+//  AppDemo
+//
+//  Created by Mohamed Salem on 24/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
